0d68acddd68a7b288857a4c6699a83926386abe4
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-31 14:10:06 +0300

    Add project files.

---
+++ Assignment.07.30.2023/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment._07._30._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number");
+            string input = Console.ReadLine();
+
+
+            int count = 0;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                count++;
+            }
+            Console.WriteLine($"Length of the number you entered is {count}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Question2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number");
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+
+            int remainder, digit = 0;
+
+
+           while (num > 0)
+            {
+                digit = num % 10;
+                if (digit % 2 == 0)
+                {
+                    Console.WriteLine($"{digit}");
+                }
+                num = (num - digit)/10;
+            }
+            Console.ReadKey();
+        }
+    }
+}
