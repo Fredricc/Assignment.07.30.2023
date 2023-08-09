7b1704a7b30583057712585a1c9e0608f4251e8e
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-08-09 09:22:26 +0300

    Factors calculator

---
+++ Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine( "Enter a number");
+            number = Convert.ToInt32(Console.ReadLine() );
+
+            Console.WriteLine("Factors of {0} ", number );
+            for(int divisor = 1; divisor <= number; divisor++) 
+            {
+                if( number % divisor == 0 )
+                {
+                    Console.Write("{0}, " , divisor );
+                }
+            }
+        }
+    }
+}
